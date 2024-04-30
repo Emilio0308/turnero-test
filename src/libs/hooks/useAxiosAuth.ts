3ee5b3724aabf7dfe7abecd559c2d1b77955335d
emilio rivas <emiliorivasruiz@gmail.com>
2024-04-29 22:25:14 -0500

    fix 8

--- src/libs/hooks/useAxiosAuth.ts
+++ src/libs/hooks/useAxiosAuth.ts
@@ -1,20 +1,14 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
-import { useLocation } from 'react-router-dom';
 import { axiosAuth } from "../axios";
-import { usePathname } from 'next/navigation'
 const useAxiosAuth = () => {
-  
   const { data: session } = useSession();
-
-
 
   useEffect(() => {
     const reqIntercept = axiosAuth.interceptors.request.use((config) => {
       if (!config.headers.Authorization) {
         config.headers.Authorization = `Bearer ${session.token}`;
-        config.headers.tenant= 'gymtest'
       }
       return config;
     });
